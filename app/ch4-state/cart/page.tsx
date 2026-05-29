220fcbbd30e4dd814f10aacd0fc169b16f354ea8
yisichoco <yisichoco@gmail.com>
2026-05-29 15:10:49 +0800

    State狀態-小型購物車-增加數量-第1種

--- app/ch4-state/cart/page.tsx
+++ app/ch4-state/cart/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useState } from 'react';
 
 const initialProducts = [
@@ -21,7 +23,18 @@ const initialProducts = [
 export default function ShoppingCart() {
   const [products, setProducts] = useState(initialProducts);
 
-  function handleIncreaseClick(productId) {}
+  function handleIncreaseClick(productId: any) {
+    // 1. 從現有的狀態拷貝出一個新的複本
+    // 深拷貝語法(超過一層以上無法用`...`來拷貝)
+    const nextProducts = JSON.parse(JSON.stringify(products));
+    // 2. 在複本上作修改
+    // 找它是在陣列的哪一個
+    const foundIndex = nextProducts.findIndex((v: any) => v.id === productId);
+    // 找到後把這個物件的count作+1的動作(理論上會找到，返回索引值)
+    nextProducts[foundIndex].count++;
+    // 3. 呼叫set方法設定給狀態
+    setProducts(nextProducts);
+  }
 
   return (
     <ul>
