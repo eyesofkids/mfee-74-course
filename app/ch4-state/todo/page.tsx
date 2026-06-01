81d261a082170bda42dbca401f934e30781aa3f6
yisichoco <yisichoco@gmail.com>
2026-06-01 10:37:14 +0800

    State狀態-待辨事項-定義狀態, map資料

--- app/ch4-state/todo/page.tsx
+++ app/ch4-state/todo/page.tsx
@@ -2,11 +2,29 @@
 
 import { useState } from 'react';
 
+// 範例資料(mock)
+const initData = [
+  {
+    id: 1,
+    text: '繳電信費',
+  },
+  { id: 2, text: '寫作業' },
+];
+
 export default function TodoPage() {
+  // 記錄待辨事項的狀態
+  const [todos, setTodos] = useState(initData);
+
   return (
     <>
       <h1>待辨事項</h1>
       <hr />
+      <input type="text" />
+      <ul>
+        {todos.map((todo) => {
+          return <li key={todo.id}>{todo.text}</li>;
+        })}
+      </ul>
     </>
   );
 }
