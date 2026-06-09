5606ffbb50a076dedce8056752449e3f7ee0628e
yisichoco <yisichoco@gmail.com>
2026-06-09 15:37:38 +0800

    Effect作用-範例-商品列表+詳細頁-列表頁

---
+++ app/proj/product-v2/page.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+// 導入next提供的有自動圖片最佳化的元件
+import Image from 'next/image';
+// 導入css樣式
+import './_styles/product-table.css';
+
+interface Product {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  stock: number;
+  image_url: string;
+  created_at: string;
+  updated_at: string;
+}
+
+// 資料來源:
+const url =
+  'https://my-json-server.typicode.com/eyesofkids/json-fake-data/products';
+
+export default function ProductTablePage() {
+  // 需要定義狀態，因為觸發更新階段(狀態需要更動才行)
+  const [products, setProducts] = useState<Product[]>([]);
+
+  // 元件第一次`渲染``之後`執行一次
+  useEffect(() => {
+    // 定義要向伺服器要商品資料的函式
+    const getProducts = async () => {
+      const res = await fetch(url);
+      const resData = await res.json();
+      setProducts(resData);
+    };
+    // 呼叫函式
+    getProducts();
+  }, []);
+
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>圖片</th>
+            <th>名稱</th>
+            <th>價格</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* 常用的map撰寫風格： 英文複數名詞 map 單數名詞 */}
+          {/* 例如: products -> product */}
+          {products.map((product) => {
+            return (
+              <tr key={product.id}>
+                <td>{product.id}</td>
+                <td>
+                  <Image
+                    src={product.image_url}
+                    width={100}
+                    height={100}
+                    alt={product.name}
+                  />
+                </td>
+                <td>{product.name}</td>
+                <td>{product.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+}
