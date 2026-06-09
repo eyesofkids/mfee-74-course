180e599aaa44e21fbd0f7c6d14ff8c218ba68c91
yisichoco <yisichoco@gmail.com>
2026-06-09 09:53:54 +0800

    Form&Refs表單_不可控表單元件-使用id作DOM查詢

--- app/ch6-form/uncontrolled/_components/my-input-id.tsx
+++ app/ch6-form/uncontrolled/_components/my-input-id.tsx
@@ -1,9 +1,23 @@
 'use client';
 
 export default function MyInputId() {
+  // 錯誤示範，因為DOM查詢和呼叫document物件的方法有副作用
+  // const inputElement = document.querySelector('#my-input')
+
   return (
     <>
       <h2>文字輸入框(使用id)</h2>
+      <input type="text" id="my-input" />
+      <button
+        onClick={() => {
+          // 正確作法，能在事件處理函式中呼叫
+          const inputElement = document.querySelector('#my-input');
+
+          alert((inputElement as HTMLInputElement).value);
+        }}
+      >
+        得到輸入框輸入值
+      </button>
     </>
   );
 }
